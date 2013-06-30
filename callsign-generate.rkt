53a069fab7e8aea796ffcefde909ef39e0f83126
Jaeden Amero <jaeden@patater.com>
2013-06-30 09:40:46 -0500

    Use char-set-contains, from srfi/14

--- callsign-generate.rkt
+++ callsign-generate.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require srfi/14) ; char-set-contains
+
 ; Return a list containing only the digits in the supplied list, maintaining
 ; order.
 (define (digits-from s)
@@ -8,7 +10,7 @@
 ; Evaluate to true iff the supplied character is a digit.
 ; If a character is not supplied, evaluate to false.
 (define (digit? c)
-  (cond ((char? c) (and (char>=? c #\0) (char<=? c #\9)))
+  (cond ((char? c) (char-set-contains? char-set:digit c))
         (#t #f)))
 
 (provide digits-from
