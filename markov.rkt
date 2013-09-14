451bfafae1b7d149b3d480e119ade2552a8fbfc5
Jaeden Amero <jaeden@patater.com>
2013-09-13 21:48:11 -0500

    Add Markov chain functions
    We can now generate word-level similar texts based on a source text.

--- markov.rkt
+++ markov.rkt
@@ -19,5 +19,41 @@
     (update-n-gram-hash! n s h position)
     h))
 
+(define (update-markov-chain-hash! n s h position)
+    (cond ((< position n) '())
+          (#t (let* ([n+1-gram (n-gram-at (+ n 1) position s)]
+                     [key (take n+1-gram n)]
+                     [next-word (last n+1-gram)])
+                (cond ((hash-has-key? h key)
+                       (hash-set! h key (cons next-word (hash-ref h key))))
+                      (#t (hash-set! h key (list next-word))))
+                (update-markov-chain-hash! n s h (- position 1))))))
+
+(define (generate-markov-chain n s)
+  (let ([position (- (length s) 1)] [h (make-hash)])
+    (update-markov-chain-hash! n s h position)
+    h))
+
+(define (traverse-markov-chain n-gram markov-chain word-limit)
+  (cond ((zero? word-limit) '())
+        ((hash-has-key? markov-chain n-gram)
+         (let ([next-word (random-from (hash-ref markov-chain n-gram))])
+           (cons next-word
+                 (traverse-markov-chain (append (cdr n-gram)
+                                                (list next-word))
+                                        markov-chain
+                                        (- word-limit 1)))))
+        (#t '())))
+
+(define (generate-similar-corpus n-gram-n corpus word-limit)
+  (let ([first-n-gram (n-gram-at n-gram-n (- n-gram-n 1) corpus)]
+        [markov-chain (generate-markov-chain n-gram-n corpus)])
+    (append first-n-gram
+            (traverse-markov-chain first-n-gram
+                                   markov-chain
+                                   (- word-limit n-gram-n)))))
+
 (provide n-gram-at
-         n-grams)
+         n-grams
+         generate-markov-chain
+         generate-similar-corpus)
